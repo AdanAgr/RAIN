60629e84fef3182c05f696c639f273c9246eba93
Adan Gonzalez Rodriguez <adan.rozaguez@gmail.com>
2024-10-02 17:45:54 +0200

    QuitarPruebaSobrante

--- prueba.py
+++
@@ -1,9 +0,0 @@
-import pandas as pd
-
-# Leer el archivo CSV
-df = pd.read_csv('movies.csv')
-
-# Obtener el valor de 'release_date' en el índice 10
-release_date = df.loc[10, 'release_date']
-
-print(f'El release_date del elemento en el índice 10 es: {release_date}')
