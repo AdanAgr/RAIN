a0b388644702bfd6cee4a53ee251f1cce5d4cbea
Tomás Rial <148789530+tomasrial46@users.noreply.github.com>
2024-11-12 19:11:41 +0100

    Delete Recomendacion directory

--- Recomendacion/Model.py
+++
@@ -1,6 +0,0 @@
-from Recommendation import Recommendation
-
-class Model:
-    def predict(self, inputData):
-        content = f"Recommended based on input: {inputData}"
-        return Recommendation(content)
