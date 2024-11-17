580244f9fd565dad21016ef9d29fb725c4706f05
Adán González <146563007+AdanAgr@users.noreply.github.com>
2024-11-17 10:59:35 +0100

    Add src

---
+++ src/peliculas_vistas.py
@@ -0,0 +1,25 @@
+import pandas as pd
+
+data_path = "./ml-10M/ratings.dat"
+
+ratings_file = f'{data_path}/ratings.dat'
+ratings = pd.read_csv(ratings_file, sep='::', engine='python', names=['userId', 'movieId', 'rating', 'timestamp'])
+
+movies_file = f'{data_path}/movies.dat'
+movies = pd.read_csv(movies_file, sep='::', engine='python', names=['movieId', 'title', 'genres'])
+
+id_to_title = dict(zip(movies['movieId'], movies['title']))
+
+def get_movies_seen_by_user(user_id):
+    seen_movies = ratings[ratings['userId'] == user_id]['movieId']
+    
+    seen_movies_titles = [id_to_title[movie_id] for movie_id in seen_movies if movie_id in id_to_title]
+    
+    return seen_movies_titles
+
+user_id = 842
+movies_seen = get_movies_seen_by_user(user_id)
+
+print(f"Películas vistas por el usuario {user_id}:")
+for title in movies_seen:
+    print(f"- {title}")
