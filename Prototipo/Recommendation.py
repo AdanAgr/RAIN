dd1039920e633a5b79a32251900331d003d38b3b
Adán González <146563007+AdanAgr@users.noreply.github.com>
2024-11-20 11:15:19 +0100

    Delete Prototipo/Recommendation.py

--- Prototipo/Recommendation.py
+++
@@ -1,7 +0,0 @@
-class Recommendation:
-    def __init__(self, title, predicted_rating):
-        self.title = title
-        self.predicted_rating = predicted_rating
-
-    def display(self):
-        print(f"Película: {self.title}, Predicción de valoración: {self.predicted_rating:.2f}")
