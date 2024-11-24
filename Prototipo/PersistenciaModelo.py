652fa3349ab36c24349c740277b8b7e6b640c894
Tomás Rial <148789530+tomasrial46@users.noreply.github.com>
2024-11-24 18:10:33 +0100

    Delete Prototipo directory

--- Prototipo/PersistenciaModelo.py
+++
@@ -1,8 +0,0 @@
-import joblib
-
-class PersistenciaModelo:
-    def guardar_modelo(self, modelo, filename):
-        joblib.dump(modelo, filename)
-        print(f"Modelo guardado en {filename}")
-
-    
